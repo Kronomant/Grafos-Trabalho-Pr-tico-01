cdd0f7dead855f3934b808c75e116bf6eb689960
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2021-08-31 22:07:25 -0300

    implementacao do grafo direcionado ponderado e nao-direcionado nao-ponderado com seus respectivos exemplos de entrada

---
+++ GDP.java
@@ -0,0 +1,114 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+class Celula {
+    public int rotulo;
+	public int elemento; // Elemento inserido na celula.
+	public Celula prox; // Aponta a celula prox.
+	/**
+	 * Construtor da classe.
+	 */
+	public Celula() {
+		this(0);
+	}
+
+	/**
+	 * Construtor da classe.
+	 * @param elemento int inserido na celula.
+	 */
+	public Celula(int rotulo) {
+      this.rotulo = rotulo;
+      this.elemento = rotulo;
+      this.prox = null;
+	}
+
+    public Celula(int rotulo, int elemento) {
+        this.rotulo = rotulo;
+        this.elemento = elemento;
+        this.prox = null;
+      }
+}
+
+
+
+class Diagrama { 
+    public int aresta;
+    public int vertices;
+    public Celula[] diagrama;
+    private int index =0;
+
+    public Diagrama(int numero_vertices){
+        this.diagrama = new Celula[numero_vertices];
+        vertices = numero_vertices;
+    }
+
+    public void addVertice(int rotulo){
+        diagrama[index]= new Celula(rotulo);
+        index++;
+    }
+
+    public void addAresta (int rotulo, int valor){
+      boolean conexao = false;
+      Celula aux = new Celula(rotulo, valor);
+
+		for (Celula i = diagrama[index-1]; conexao != true; i = i.prox) {
+			if (i.prox == null) {
+				i.prox = aux;
+				conexao = true;
+			}
+		}
+    }
+
+    public void imprimeVertices(){
+
+        for(int j = 0; j < vertices; j++){
+            System.out.print("("+diagrama[j].rotulo+") ");
+            if(diagrama[j].prox != null){
+                for (Celula g = diagrama[j].prox; g != null; g = g.prox) {
+                    System.out.print("---"+g.elemento+"--> "+"("+g.rotulo+")");
+					if (g.prox == null) {
+						System.out.print("\n");
+					}
+					else {
+						System.out.print(" ");
+					}	
+            }
+        }  else {
+            System.out.println("_");
+        }   
+     
+    }
+
+}
+}
+
+public class GDP{
+    public static void lerEntrada(Diagrama diagrama){
+        try{
+            FileReader arq = new FileReader("/home/milard/Documentos/Faculdade/Grafos/TP01/entrada.txt");
+            BufferedReader lerArq = new BufferedReader(arq);
+
+            String linha = lerArq.readLine(); 
+            while (linha != null) {
+            
+                String[] dados = linha.split(",");
+                 if(dados[0].equals("V")){
+                     diagrama.addVertice(Integer.parseInt(dados[1]));
+                 } else if(dados[0].equals("A")){
+                    diagrama.addAresta(Integer.parseInt(dados[1]), Integer.parseInt(dados[2]));
+                 } 
+                linha = lerArq.readLine();
+            }
+            arq.close();
+        } catch(IOException erro){
+            System.err.printf("Erro na abertura do arquivo: %s.\n", erro.getMessage());
+        }
+    }
+    public static void main(String[] args){
+        Diagrama diagrama = new Diagrama(4);
+        lerEntrada(diagrama);
+        diagrama.imprimeVertices();
+    }
+
+}
---
+++ GNDNP.java
@@ -0,0 +1,50 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class GNDNP {
+    public static void main(String[] args) {
+    
+        try{
+            FileReader arq = new FileReader("/home/milard/Documentos/Faculdade/Grafos/TP01/entrada2.txt");
+            BufferedReader lerArq = new BufferedReader(arq);
+
+            String linha = lerArq.readLine(); 
+            int matrizAdjacencia[][] = new int[Integer.parseInt(linha)][Integer.parseInt(linha)]; // pega quantidade de vertices
+            linha = lerArq.readLine(); 
+            while (linha != null) {
+            
+                String[] dados = linha.split(",");   
+                matrizAdjacencia[Integer.parseInt(dados[0])-1][Integer.parseInt(dados[1])-1] = 1;
+                matrizAdjacencia[Integer.parseInt(dados[1])-1][Integer.parseInt(dados[0])-1] = 1;
+                linha = lerArq.readLine();
+            }
+            arq.close();
+
+            for(int i = 0; i< 4; i++){
+                for(int j =0; j< 4; j++)
+                {
+                    System.out.print(matrizAdjacencia[i][j] + " ");
+                }
+                System.out.println(" ");
+            }
+
+
+            for(int i = 0; i< 4; i++){
+                for(int j =0; j< 4; j++)
+                {
+                    if(matrizAdjacencia[i][j] == 1){
+                        System.out.println("("+(i+1)+") ---- ("+ (j+1) + ")");
+                    }
+                  
+                }
+                // System.out.println(" ");
+            }
+
+
+        } catch(IOException erro){
+            System.err.printf("Erro na abertura do arquivo: %s.\n", erro.getMessage());
+        }
+        
+    }
+}
